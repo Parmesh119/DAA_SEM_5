af873a93f9e89300b00b7ba2b39a1b3f457c8dff
Parmesh Bhatt <101285245+Parmesh119@users.noreply.github.com>
2023-06-27 15:44:10 +0530

    Update Recursive.cpp

--- Practical-1/Factorial_Analysis/Recursive/Recursive.cpp
+++ Practical-1/Factorial_Analysis/Recursive/Recursive.cpp
@@ -1,1 +1,25 @@
-
+#include <iostream>
+using namespace std;
+int  cnt=0;
+int fact(int n) {
+    cnt++;
+   if ((n==1)){
+    cnt++;
+       return 1;
+   }
+   cnt++;
+   cnt++;
+   cnt++;
+    return n*fact(n-1);
+       
+}
+int main() {
+   int n;
+   cout<<"n: ";
+   cin>>n;
+   cnt++;
+   cout<<fact(n)<<endl;
+   cnt++;
+   cout<<"Counter is: "<<cnt<<endl;
+   return 0;
+}
