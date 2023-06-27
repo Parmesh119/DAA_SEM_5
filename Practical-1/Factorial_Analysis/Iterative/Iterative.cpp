eb822b1e2742023d42dc155648e2d914c3f84d91
Parmesh Bhatt <101285245+Parmesh119@users.noreply.github.com>
2023-06-27 15:42:51 +0530

    Update Iterative.cpp

--- Practical-1/Factorial_Analysis/Iterative/Iterative.cpp
+++ Practical-1/Factorial_Analysis/Iterative/Iterative.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"n: ";
+    cin>>n;
+    int fact = 1, cnt=0;
+    cnt++;
+    cnt++;
+    for(int i=1; i<=n; i++){
+       cnt++;
+       cnt++;
+       fact = fact * i;
+       cnt++;
+    }
+    cnt++;
+    cout<<"The Factorial is: "<<fact<<endl;
+    cnt++;
+    cout<<"The Counter is: "<<cnt<<endl;
+    return 0;
+}
