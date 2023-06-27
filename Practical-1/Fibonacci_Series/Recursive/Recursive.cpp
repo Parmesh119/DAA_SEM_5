c10793475c7b77083a306eb38db0db6ac0d1908e
Parmesh Bhatt <101285245+Parmesh119@users.noreply.github.com>
2023-06-27 16:02:31 +0530

    Update Recursive.cpp

--- Practical-1/Fibonacci_Series/Recursive/Recursive.cpp
+++ Practical-1/Fibonacci_Series/Recursive/Recursive.cpp
@@ -1,1 +1,34 @@
-
+#include <iostream>
+using namespace std;
+int cnt=0;
+int fib(int x) {
+   if((x==1)||(x==0)) {
+       cnt++;
+       cnt++;
+       cnt++;
+      return(x);
+   }
+   cnt++;
+   cnt++;
+   cnt++;
+   cnt++;
+    return(fib(x-1)+fib(x-2));
+}
+int main() {
+   int x , i=0;
+   cnt++;
+   cout << "Enter the number of terms of series : ";
+   cin >> x;
+   cout << "\nFibonnaci Series : ";
+   cnt++;
+   while(i < x) {
+      cout << " " << fib(i);
+      cnt++;
+      cnt++;
+      cnt++;
+      i++;
+   }
+   cnt++;
+   cout<<"The Counter is: "<<cnt;
+   return 0;
+}
